80670b3e86917b7564531dad772d0b538c2bfe95
StandardGage <gagedolph@yahoo.com>
2023-02-10 13:35:15 -0500

    task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Row, Container } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -7,10 +8,36 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 -Gage Schuster with React Hooks and TypeScript
             </header>
+            <h1>Hello There</h1>
+            <img
+                src="https://picsum.photos/id/238/200/300"
+                alt="pic didn't load"
+            ></img>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+            <ul>
+                <li>list item</li>
+                <li>list item</li>
+                <li>list item</li>
+                <li>list item</li>
+                <li>list item</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            {/* <header>Container</header> */}
+            <Container className="Container">
+                <Row>
+                    <Col>
+                        <div className="red-box"></div>
+                    </Col>
+                    <Col>
+                        <div className="red-box"></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
