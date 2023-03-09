6c1a8f695e2647b0a0494b33b5e0f8d56b0686df
StandardGage <gagedolph@yahoo.com>
2023-03-09 16:18:36 -0500

    Finished ChangeType and Start Attempt

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+
+    return (
+        <>
+            <Button
+                onClick={() =>
+                    type === "short_answer_question"
+                        ? setType("multiple_choice_question")
+                        : setType("short_answer_question")
+                }
+            >
+                Change Type
+            </Button>
+            <div>
+                {type === "short_answer_question"
+                    ? "Short Answer"
+                    : "Multiple Choice"}
+            </div>
+        </>
+    );
 }
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,30 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState(4);
+    const [inProgress, setProgress] = useState(false);
+
+    return (
+        <>
+            <Button
+                disabled={inProgress || attempts == 0}
+                onClick={() => {
+                    setProgress(true);
+                    setAttempts(attempts - 1);
+                }}
+            >
+                Start Quiz
+            </Button>
+            <Button disabled={!inProgress} onClick={() => setProgress(false)}>
+                Stop Quiz
+            </Button>
+            <Button
+                disabled={inProgress}
+                onClick={() => setAttempts(attempts + 1)}
+            >
+                Mulligan
+            </Button>
+            <div>{attempts}</div>
+        </>
+    );
 }
